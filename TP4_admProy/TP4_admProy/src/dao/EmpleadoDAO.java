ba73b904670a7ff845fea69e9a2ca0a1fdb64df7
Kevin Haunstetter <kevinhaunstetter@gmail.com>
2018-01-07 12:49:17 -0300

    gj

---
+++ TP4_admProy/TP4_admProy/src/dao/EmpleadoDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import java.util.List;
+
+import Exceptions.MyDAOExcepcion;
+import entidades.Empleado;
+
+/**
+ * Definimos la interface DAO, desde aca hacemos la implementacion para establecer con lenguaje SQL 
+ * como vamos a impactar en la DB.
+ *
+ */
+public interface EmpleadoDAO {
+
+	public void insertEmpleado(Empleado e) throws MyDAOExcepcion;
+	public void deleteEmpleadoById(Empleado e) throws MyDAOExcepcion;
+	public void updateEmpleado(Empleado e) throws MyDAOExcepcion;
+	public boolean getEmpleadoById(Empleado e) throws MyDAOExcepcion;
+	public List<Empleado>  getAllEmpleados() throws MyDAOExcepcion;	
+
+}
+
